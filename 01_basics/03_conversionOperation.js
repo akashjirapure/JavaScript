da4673d65c7ea7e4f3ad333c7fbc3a64b3a0c5ed
akashjirapure <129483589+akashjirapure@users.noreply.github.com>
2025-01-11 09:53:45 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = null
+// let score1 = "33abc"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let getValueInNumber = Number(score)
+// console.log("Type :",typeof getValueInNumber)
+// // After conversin
+// console.log(getValueInNumber) // let getValueInNumber = null  Output : 0
+
+// let getValueInNumber1 = Number(score1)
+// console.log("Type :",typeof getValueInNumber)
+// // After conversin
+// console.log(getValueInNumber1) //let getValueInNumber = "33abc" Output : NaN
+
+// Conversion in Number
+// "33" => 33
+// "33abc" => NaN = Not a Number
+//  Ture  => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = "123"
+
+let boolean = Boolean (isLoggedIn)
+
+console.log("Type of Boolean : " , typeof boolean)
+console.log("Result : ", boolean);
+
+// 1 = true , 0 = false
+// "" = false
+// "akash" = true
+
+let number = 33
+
+let letter = String(number)
+console.log(typeof letter);
+
+console.log(letter);
